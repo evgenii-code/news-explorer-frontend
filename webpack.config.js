6d4300383526b7e4fde6fac4bf03a61055df4eb8
evgenii-code <osipenkov.evgenii@gmail.com>
2020-09-25 16:43:18 +0300

    html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: { main: './src/index.js' },
@@ -22,5 +23,12 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin({ filename: 'style.css' }),
+    new HtmlWebpackPlugin({
+      // Означает, что:
+      inject: false, // стили НЕ нужно прописывать внутри тегов
+      hash: true, // для страницы нужно считать хеш
+      template: './src/index.html', // откуда брать образец для сравнения с текущим видом проекта
+      filename: 'index.html', // имя выходного файла, то есть того, что окажется в папке dist после сборки
+    }),
   ],
 };
