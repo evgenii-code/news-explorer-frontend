a5a50d67551a35020bbf972ccf2e53e8b101cea9
evgenii-code <osipenkov.evgenii@gmail.com>
2020-10-21 18:11:22 +0300

    NewsCards refactor

--- src/js/components/NewsCard.js
+++ src/js/components/NewsCard.js
@@ -1,27 +1,40 @@
-import BaseComponent from './BaseComponent';
-
-export default class NewsCard extends BaseComponent {
+export default class NewsCard {
   // Класс карточки новости
   constructor({
-    selectors, element, handlers, content, dateFormatter,
+    selector, config, content, dateFormatter, iconClickHandler,
   }) {
-    super({ element, handlers });
+    this._element = document.querySelector(selector).content.cloneNode(true).firstElementChild;
+    this._config = config;
     this._content = content;
-    this._selectors = selectors;
-    this._title = this._element.querySelector(this._selectors.title);
-    this._address = this._element.querySelector(this._selectors.address);
-    this._publishedAt = this._element.querySelector(this._selectors.publishedAt);
-    this._description = this._element.querySelector(this._selectors.description);
-    this._urlToImage = this._element.querySelector(this._selectors.urlToImage);
-    this._dateFormatter = dateFormatter;
+    this._title = this._element.querySelector(this._config.title);
+    this._address = this._element.querySelector(this._config.address);
+    this._publishedAt = this._element.querySelector(this._config.publishedAt);
+    this._dateFormatter = dateFormatter || ((date) => date);
+    this._description = this._element.querySelector(this._config.description);
+    this._urlToImage = this._element.querySelector(this._config.urlToImage);
+    this._iconContainer = this._element.querySelector(this._config.iconContainer);
+    this._icon = this._element.querySelector(this._config.icon);
+    this._iconClickHandler = iconClickHandler.bind(this);
+  }
+
+  _addKeyword() {
+    this._keyword = document.createElement('p');
+    this._keyword.classList.add(this._config.keyword);
+    this._keyword.textContent = 'Слово';
+    this._element.prepend(this._keyword);
   }
 
   create() {
     this._title.textContent = this._content.title;
     this._address.textContent = this._content.source.name;
     this._publishedAt.textContent = this._dateFormatter(this._content.publishedAt);
+    this._publishedAt.setAttribute('datetime', this._content.publishedAt.slice(0, 10));
     this._description.textContent = this._content.description;
     this._urlToImage.src = this._content.urlToImage;
+    // this._addKeyword();
+
+    this._setListeners();
+
     return this._element;
   }
 
@@ -30,11 +43,32 @@ export default class NewsCard extends BaseComponent {
     return this._element;
   }
 
-  // renderIcon(){
-  // // отвечает за отрисовку иконки карточки.
-  // // У этой иконки три состояния:
-  // // - иконка незалогиненного пользователя,
-  // // - активная иконка залогиненного,
-  // // - неактивная иконка залогиненного
-  // }
+  _setListeners() {
+    this._icon.addEventListener('click', this._iconClickHandler);
+  }
+
+  _removeListeners() {
+    this._icon.removeEventListener('click', this._iconClickHandler);
+  }
+
+  renderIcon({ isLoggedIn, marked }) {
+    // отвечает за отрисовку иконки карточки.
+    // У этой иконки три состояния:
+    // - иконка незалогиненного пользователя,
+    // - активная иконка залогиненного,
+    // - неактивная иконка залогиненного
+
+    if (isLoggedIn) {
+      this._iconContainer.classList.remove(this._config.iconContainerUnauth);
+      if (marked) {
+        this._icon.classList.add(this._config.iconMarked);
+        this._icon.classList.remove(this._config.iconUnmarked);
+      } else {
+        this._icon.classList.remove(this._config.iconMarked);
+        this._icon.classList.add(this._config.iconUnmarked);
+      }
+    } else {
+      this._iconContainer.classList.add(this._config.iconContainerUnauth);
+    }
+  }
 }
