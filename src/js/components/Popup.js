e1f8bb49c564325d153b12148c42a57278b47b5b
evgenii-code <osipenkov.evgenii@gmail.com>
2020-10-21 18:12:40 +0300

    Popup refactor

--- src/js/components/Popup.js
+++ src/js/components/Popup.js
@@ -1,11 +1,11 @@
 export default class Popup {
   constructor({
-    selector, config, container, forms,
+    selector, config, container, content,
   }) {
     this._element = document.querySelector(selector).content.cloneNode(true).firstElementChild;
     this._config = config;
-    this._contentElement = this._element.querySelector(this._config.container);
-    this._forms = forms;
+    this._contentContainerElement = this._element.querySelector(this._config.container);
+    this._content = content;
     this._closeButton = this._element.querySelector(this._config.closeButton);
     this._choiceButton = this._element.querySelector(this._config.choiceButton);
     this._title = this._element.querySelector(this._config.title);
@@ -19,12 +19,12 @@ export default class Popup {
 
   setContent(element) {
   // вставляет в попап содержимое, например, форму входа или сообщение об успешной регистрации
-    this._contentElement.append(element);
+    this._contentContainerElement.append(element);
   }
 
   clearContent() {
   // очищает содержимое попапа
-    this._contentElement.innerHTML = '';
+    this._contentContainerElement.innerHTML = '';
   }
 
   open() {
@@ -45,23 +45,25 @@ export default class Popup {
 
   _setDefaultContent() {
     this.clearContent();
-    this.setContent(this._forms.signin);
+    this.setContent(this._content.signinElement);
   }
 
   _choiceClickHandler(event) {
     event.preventDefault(event);
 
-    const formType = this._contentElement.querySelector(this._config.form).dataset.contentType;
+    const formType = this._contentContainerElement.querySelector(this._config.form)
+      .dataset.contentType;
+
     this.clearContent();
 
     if (formType === 'signin') {
       this._title.textContent = this._config.signupTitle;
       this._choiceButton.textContent = this._config.signupChoiceText;
-      this.setContent(this._forms.signup);
+      this.setContent(this._content.signupElement);
     } else {
       this._title.textContent = this._config.signinTitle;
       this._choiceButton.textContent = this._config.signinChoiceText;
-      this.setContent(this._forms.signin);
+      this.setContent(this._content.signinElement);
     }
   }
 
