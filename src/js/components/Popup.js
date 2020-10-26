2cb4dd38c6b5dc0f3d3d9ec463ce12d79bc5459b
evgenii-code <osipenkov.evgenii@gmail.com>
2020-10-26 21:11:26 +0300

    popup closed by esc button

--- src/js/components/Popup.js
+++ src/js/components/Popup.js
@@ -1,26 +1,29 @@
+import ESC_KEYCODE from '../constants/key-codes';
+
 export default class Popup {
   constructor({
-    selector, config, content, toggleMenu,
+    selector, config, toggleMenu,
   }) {
     this._config = config;
     this._parentContainer = document.querySelector(this._config.parentContainer);
     this._element = this._parentContainer.querySelector(selector)
       .content.cloneNode(true).firstElementChild;
+    this._background = this._element.querySelector(this._config.background);
     this._contentContainerElement = this._element.querySelector(this._config.container);
-    this._content = content;
     this._closeButton = this._element.querySelector(this._config.closeButton);
     this._choiceButton = this._element.querySelector(this._config.choiceButton);
     this._title = this._element.querySelector(this._config.title);
     this._toggleMenu = toggleMenu;
     this._closeButtonClickHandler = this._closeButtonClickHandler.bind(this);
-    this._choiceClickHandler = this._choiceClickHandler.bind(this);
-    this._setHandlers();
+    this._handleEscUp = this._handleEscUp.bind(this);
     this.open = this.open.bind(this);
     this.close = this.close.bind(this);
   }
 
-  setContent(element) {
+  setContent(element, nextElement) {
   // вставляет в попап содержимое, например, форму входа или сообщение об успешной регистрации
+    this._currentContentElement = element;
+    this._nextContentElement = nextElement;
     this._contentContainerElement.append(element);
   }
 
@@ -30,51 +33,41 @@ export default class Popup {
   }
 
   open() {
-    this._setDefaultContent();
-
     this._toggleMenu();
-
+    this._setHandlers();
     this._parentContainer.append(this._element);
   }
 
   close() {
     this._toggleMenu();
-
+    this._removeHandlers();
     this._element.remove();
   }
 
   _closeButtonClickHandler(event) {
-    event.preventDefault(event);
+    // event.preventDefault(event);
 
-    this.close();
-  }
+    const { target } = event;
 
-  _setDefaultContent() {
-    this.clearContent();
-    this.setContent(this._content.signinElement);
-  }
-
-  _choiceClickHandler(event) {
-    event.preventDefault(event);
-
-    const formType = this._contentContainerElement.querySelector(this._config.form)
-      .dataset.contentType;
-
-    this.clearContent();
-
-    if (formType === 'signin') {
-      this._title.textContent = this._config.signupTitle;
-      this._choiceButton.textContent = this._config.signupChoiceText;
-      this.setContent(this._content.signupElement);
-    } else {
-      this._title.textContent = this._config.signinTitle;
-      this._choiceButton.textContent = this._config.signinChoiceText;
-      this.setContent(this._content.signinElement);
+    if (target.classList.contains(this._config.backgroundClass)
+    || target.classList.contains(this._config.closeButtonClass)) {
+      this.close();
     }
   }
 
+  _handleEscUp(event) {
+    event.preventDefault();
+
+    if (event.which === ESC_KEYCODE) this.close();
+  }
+
   _setHandlers() {
-    this._closeButton.addEventListener('click', this._closeButtonClickHandler);
-    this._choiceButton.addEventListener('click', this._choiceClickHandler);
+    this._element.addEventListener('click', this._closeButtonClickHandler);
+    document.addEventListener('keyup', this._handleEscUp);
+  }
+
+  _removeHandlers() {
+    this._element.removeEventListener('click', this._closeButtonClickHandler);
+    document.removeEventListener('keyup', this._handleEscUp);
   }
 }
